c1a4057b29465e147d9f58c4b0cc10710fff1b2f
OM <oli90martin@web.de>
2024-03-06 19:28:57 +0100

    LightProbes and Reflections

--- Lighting/Assets/Scripts/LightIntensityAndColorController.cs
+++ Lighting/Assets/Scripts/LightIntensityAndColorController.cs
@@ -7,23 +7,51 @@ public class LightIntensityAndColorController : MonoBehaviour
     //[SerializeField] private Light[] pointLights;
     [SerializeField] private GameObject[] pointLights;
     [SerializeField] private GameObject player;
+    [SerializeField] private float maxDistance = 10f;
+    [SerializeField] private float maxIntensity = 1f;
+    [SerializeField] private Color startColor = Color.red;
+    [SerializeField] private Color endColor = Color.green;
 
-    // Start is called before the first frame update
+
     void Start()
     {
         //pointLights = FindObjectsOfType<Light>();
         pointLights = GameObject.FindGameObjectsWithTag("PointLight");
         player = GameObject.Find("Player");
-
-      
     }
 
     void Update()
     {
       foreach(GameObject pointLight in pointLights)
       {
+            Light light = pointLight.GetComponent<Light>();
+            if(pointLights.Length > 0)
+            {
+                UpdateLightProperties(light);
+            }
+        }
+    }
 
-      }
-      
+    private void UpdateLightProperties(Light pointLight)
+    {
+        float distance = Vector3.Distance(pointLight.transform.position, player.transform.position);
+
+        //max distance bei der die Lampe reagiert
+        if(distance < maxDistance)
+        {
+            //Intensität anpassen
+            float intensityValue = (1 - (distance / maxDistance)) * maxIntensity;
+            pointLight.intensity = intensityValue;
+
+            //Color anpassen
+            float colorRatio = (1 - (distance / maxDistance));
+            pointLight.color = Color.Lerp(startColor, endColor, colorRatio);
+
+        }
+        else
+        {
+            pointLight.intensity = 0f;
+            pointLight.color = startColor;
+        }
     }
 }
